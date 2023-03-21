42babdaca0aa5697d52deb8a764f5b8811966eef
tucuvaldez <105434510+tucuvaldez@users.noreply.github.com>
2023-03-21 12:42:03 +0100

    message added

--- src/app.js
+++ src/app.js
@@ -7,5 +7,5 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, console.log(`Listening on ${PORT}`));
 
 app.get("/", (req, res) => {
-  res.send(`Hola Railway`);
+  res.send(`Hello Railway App`);
 });
